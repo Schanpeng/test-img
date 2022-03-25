bc749311f85dcdd69afd28de4679fa84d1ec264a
Schanpeng <78044526+Schanpeng@users.noreply.github.com>
2022-03-25 14:39:01 -0400

    added comments for sell.php file

--- sell.php
+++ sell.php
@@ -1,5 +1,6 @@
 
 <?php
+//initiate database connection
 require_once 'dbConfig.php';
 
 // If file upload form is submitted 
@@ -13,7 +14,7 @@ require_once 'dbConfig.php';
     $description = $_POST['description'];
     $imagefile = $_POST['image'];
 
-
+    //check image file not empty (still need to add more but its working atm)
     if(!empty($_FILES['image']['tmp_name'])) { 
 
         $imgData = addslashes(file_get_contents($_FILES['image']['tmp_name']));
@@ -22,7 +23,7 @@ require_once 'dbConfig.php';
         // Insert variable content into database 
         $insert = $db->query("INSERT INTO sell(name, price, quantity, seller, description, image) 
         VALUES ('$name', '$price', '$quantity', '$seller', '$description' ,'$imgData')"); 
-
+        //if sql is ran correctly, show msg
         if($insert){
             echo ' nice, listing successful';
 
